211d3acf37392d2a2f86d536bc707e9f146b10b9
Luki1tahh <lucasolg23@gmail.com>
2025-11-22 17:21:44 -0300

    agregado de portfolioYT

---
+++ yt/web/src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='min-h-screen'>
+      {/* Theme toggle */}
+
+      {/* Background effect */}
+
+      {/* Navbar */}
+
+      {/* Main Content */}
+
+      {/* Footer */}
+    </div>
+  )
+}
+
+export default Home
