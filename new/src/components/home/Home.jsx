11568a388be26f831a7354c82cb652bb415bf12f
supportGD <helpdesk@goldmund.com.ar>
2025-12-16 10:20:53 -0300

    acomodamiento

--- new/src/components/home/Home.jsx
+++ new/src/components/home/Home.jsx
@@ -1,13 +1,9 @@
 import React from 'react'
-import Presentacion from '../cards/Presentacion'
-import Projects from '../cards/Projects'
-import './home.css'
 
 const Home = () => {
   return (
-    <div class='bg-black pt-50 pb-50 pl-80 pr-80'>
-      <Presentacion />
-      <Projects />
+    <div class='bg-black min-h-screen'>
+      
     </div>
   )
 }
