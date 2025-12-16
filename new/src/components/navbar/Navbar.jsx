e44ec83390b547fd2939c7786fa138fae49795b6
supportGD <helpdesk@goldmund.com.ar>
2025-12-16 10:41:36 -0300

    upgrade tailwindcss

--- new/src/components/navbar/Navbar.jsx
+++ new/src/components/navbar/Navbar.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div class='bg-black flex justify-around p-15'>
+    <div class='bg-black flex justify-around p-4'>
 
-        <h1 class='text-white text-6xl'> Lucas Olguin </h1>
+        <h1 class='text-white text-4xl'> Lucas Olguin </h1>
 
       <div class='flex text-white list-none gap-4'>
-        <li class='bg-gray-800 text-2xl p-6 rounded-3xl'>Acerca de mi</li>
-        <li class='bg-gray-800 text-2xl p-6 rounded-3xl'>Proyectos</li>
-        <li class='bg-gray-800 text-2xl p-6 rounded-3xl'>Contacto</li>
+        <li class=''>Acerca de mi</li>
+        <li class=''>Proyectos</li>
+        <li class=''>Contacto</li>
       </div>
 
     </div>
